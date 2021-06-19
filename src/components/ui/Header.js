11a123a488f97a8b011d4edc316c1bf9ec262b88
sriramcodes <sriramcodes@gmail.com>
2021-06-19 14:46:47 -0700

    Added reference/tutorial info

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import logo from '../../images/logo.png'
+import '../ui/Header.css'
 
 const Header = () => {
     return (
         <header className='center'>
             <img src={logo} alt='' />
+            <span>A learning experience following <a href="https://www.youtube.com/watch?v=YaioUnMw0mo">this tutorial by Traversy Media</a></span>
         </header>
     )
 }
